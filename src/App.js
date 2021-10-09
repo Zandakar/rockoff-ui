f1bae066bbccc59911f356740e2b6f066bf20dd0
Tom <tom.deruijter@hotmail.com>
2021-10-09 22:17:09 +0800

    working message round trip

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-const client = new W3CWebSocket("ws://54.206.45.48:8000");
+const address = "ws://54.206.45.48:8000";
 
-// class App extends Component {
-//   componentWillMount() {
-//     client.onopen = () => {
-//       console.log("WebSocket Client Connected");
-//     };
-//     client.onmessage = (message) => {
-//       console.log(message);
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         Practical Intro To WebSockets.
-//         <div>test</div>
-//       </div>
-//     );
-//   }
-// }
+const client = new W3CWebSocket(address);
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
-    client.onopen = () => {
+    client.onopen = (connection) => {
       console.log("WebSocket Client Connected");
     };
     client.onmessage = (message) => {
+      console.log("recieved message from socket");
       console.log(message);
+      setMessages([...messages, message]);
     };
   });
+
+  const handleButtonPress = () => {
+    console.log("clicky");
+    client.send("bla");
+  };
+
+  const createMessageDivs = () =>
+    messages.map((message) => {
+      return <div>{message.data}</div>;
+    });
 
   return (
     <div>
       Practical Intro To WebSockets.
       <div>test2</div>
+      <button onClick={() => handleButtonPress()}>send message</button>
+      {createMessageDivs()}
     </div>
   );
 }
