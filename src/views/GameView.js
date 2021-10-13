091ab7f0b5478468fb0ca3ebb2b9be31ce8fa87f
Tom <tom.deruijter@hotmail.com>
2021-10-13 15:32:03 +0800

    added copy to clipboard

--- src/views/GameView.js
+++ src/views/GameView.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { COMMANDS } from "../providers/WSProvider";
 import { useParams } from "react-router-dom";
 
 export default function Gameview({ sendMessage }) {
+  const [copiedToClipBoard, setCopiedToClipboard] = useState(false);
   const gameUrl = window.location.href;
   const gameId = useParams()[0];
 
@@ -17,6 +18,18 @@ export default function Gameview({ sendMessage }) {
       {`Waiting for a mate...`}
       <div>{`Share this link with the friend you want to vs:`}</div>
       <div>{`${gameUrl}`}</div>
+      <button
+        onClick={() => {
+          navigator.clipboard.writeText(gameUrl);
+          setCopiedToClipboard(true);
+          setTimeout(() => {
+            setCopiedToClipboard(false);
+          }, 500);
+        }}
+      >
+        Copy to clipboard
+      </button>
+      {copiedToClipBoard && <div>copied</div>}
     </div>
   );
 }
